fbe868a354a02f0e8261e3bdca5f0f49d0f75bde
cory cathearll <ccatherall15@gmail.com>
2020-12-01 21:49:41 -0500

    adding second solution

--- aoc-day-one.js
+++ aoc-day-one.js
@@ -1,6 +1,20 @@
+
+function twoSumProduct(array, target = 2020) {
+    let cache = {};
+    for (const num of array) {
+      let y = target - +num 
+      if (y in cache) {
+        return +num * y
+      } else {
+        cache[+num] = true;
+      }
+    }
+    return false;
+  }
+  
+
 function threeSumProduct(array, target = 2020){
     array.sort((a, b) => a - b);
-    
     for (let i = 0; i < array.length; i++) {
       let start = i + 1, end = array.length - 1;
       while (start < end) {
@@ -14,4 +28,5 @@ function threeSumProduct(array, target = 2020){
         }
       }
     }
+    return false
   }
